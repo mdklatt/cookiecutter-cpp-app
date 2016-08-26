5c0eecd2ca1b80bcc9375d641707e5afdfebf86f
Michael Klatt <mdklatt@ou.edu>
2016-08-25 23:41:27 -0500

    replaced test.bash with tests/test_template.py

---
+++ tests/test_template.py
@@ -0,0 +1,56 @@
+""" Test the cpp-app Cookiecutter template.
+
+A template project is created in a temporary directory, the application and its
+test suite are built, and both are executed.
+
+"""
+from contextlib import contextmanager
+from json import load
+from os import chdir
+from os import getcwd
+from os.path import abspath
+from os.path import dirname
+from os.path import join
+from shlex import split
+from shutil import rmtree
+from subprocess import check_call
+from tempfile import mkdtemp
+
+from cookiecutter.main import cookiecutter
+
+
+def main():
+    """ Execute the test.
+    
+    """
+    @contextmanager
+    def tmpdir():
+        """ Enter a self-deleting temporary directory. """
+        cwd = getcwd()
+        tmp = mkdtemp()
+        try:
+            chdir(tmp)
+            yield tmp
+        finally:
+            rmtree(tmp)
+            chdir(cwd)
+        return
+
+    template = dirname(dirname(abspath(__file__)))
+    defaults = load(open(join(template, "cookiecutter.json")))
+    with tmpdir():
+        cookiecutter(template, no_input=True)
+        chdir(defaults["project_name"])
+        check_call(split("cmake -DCMAKE_BUILD_TYPE=Debug"))
+        check_call(split("cmake --build ."))
+        app = defaults["project_name"]
+        print("build/Debug/test_{:s}".format(app))
+        check_call(split("build/Debug/test_{:s}".format(app)))
+        check_call(split("build/Debug/{:s}".format(app)))
+    return 0
+    
+    
+# Make the script executable.
+
+if __name__ == "__main__":
+    raise SystemExit(main())
