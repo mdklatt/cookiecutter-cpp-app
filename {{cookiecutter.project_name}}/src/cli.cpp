3c8db97d9d378133bf43b6fc40384dba61fece8c
Michael Klatt <mdklatt@alumni.ou.edu>
2017-04-23 21:23:51 -0500

    refactored cli module to use getopt library

--- {{cookiecutter.project_name}}/src/cli.cpp
+++ {{cookiecutter.project_name}}/src/cli.cpp
@@ -1,11 +1,26 @@
 /// Implementation of the {{ cookiecutter.project_name}} CLI.
 ///
 /// @file
+#include <getopt.h>
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include "core/CommandLine.hpp"
 #include "api/api.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+
+/// Display a usage message.
+///
+void usage()
+{
+    cout << "{{ cookiecutter.app_name }} [-h]" << endl;
+    return;
+}
 
 
 /// Entry point for the command line interface.
@@ -18,31 +33,30 @@
 /// @return application exit code
 int cli(int argc, char* argv[])
 {
-    CommandLine cmdl{true};  // enable strict argument parsing
-    cmdl.opt("version", 'v');
-    auto sub1(cmdl.sub("cmd1"));
-    sub1.pos("arg");
-    auto sub2(cmdl.sub("cmd2"));
-    sub2.pos("arg");
-    
-    cmdl.parse(argc, argv);
-    if (cmdl.has_arg("version")) {
-        // TODO: Implement project versioning.
-        std::cout << "{{ cookiecutter.project_name }} x.x.x" << std::endl;
-        return EXIT_SUCCESS;
+    // The leading '+' for short_opts enables POSIXLY_CORRECT behavior, which
+    // halts option processing as soon as a non-option is encountered. This is
+    // necessary for implementing subcommands.
+    const char* short_opts{"+:h"};
+    const option long_opts[]{
+        {"help", no_argument, nullptr, 'h'},
+        {nullptr, 0, nullptr, 0}  // sentinel
+    };
+    while (true) {
+        // Process options.
+        const int opt{getopt_long(argc, argv, short_opts, long_opts, nullptr)};
+        if (opt == -1) {
+            break;
+        }
+        switch (opt) {        
+            case 'h': 
+                usage();
+                return EXIT_SUCCESS;
+            case '?':  // unknown option
+            case ':':  // missing option value
+            default:
+                usage();
+                return EXIT_FAILURE;
+        }
     }
-    if (cmdl.has_arg("cmd1")) {
-        const auto arg(cmdl["arg"].front());
-        cmd1(arg);
-    }
-    else if (cmdl.has_arg("cmd2")) {
-        const auto arg(cmdl["arg"].front());
-        cmd2(arg);    
-    }
-    else {
-        // Missing subcommand.
-        std::cout << cmdl.usage() << std::endl;
-    }
-    
     return EXIT_SUCCESS;
 }
---
+++ {{cookiecutter.project_name}}/test/test_cli.cpp
@@ -0,0 +1,80 @@
+/// Test suite for the cli module.
+///
+/// Link all test files with the `gtest_main` library to create a command-line 
+/// test runner.
+///
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <gtest/gtest.h>
+
+using std::string;
+using std::vector;
+using testing::Test;
+
+
+extern int cli(int, char* []);  // cli.cpp
+
+
+/// Test fixture for the cli module test suite.
+///
+/// This is used to group tests and provide common set-up and tear-down
+/// code. A new test fixture is created for each test to prevent any side 
+/// effects between tests. Member variables and methods are injected into
+/// each test that uses this fixture.
+///
+class CliTest: public Test
+{
+protected:
+    /// Set up test fixture.
+    ///
+    CliTest() {}   
+    
+    /// Tear down the test fixture.
+    ///
+    ~CliTest() 
+    { 
+        dealloc(); 
+    }
+
+    /// Set command-line arguments.
+    ///
+    void cmdl(const vector<string>& args)
+    {
+        dealloc();
+        argc = static_cast<int>(args.size());
+        argv = new char*[argc];
+        auto iter(args.begin());
+        for (int pos{0}; pos < argc; ++pos, ++iter) {
+            const size_t len{iter->size() + 1};
+            argv[pos] = new char[len];
+            assert(strncpy(argv[pos], iter->c_str(), len) != nullptr);
+        }
+        return;
+    }
+    
+    int argc{0};
+    char** argv{nullptr};
+    
+private:
+    /// Deallocate argv.
+    ///
+    void dealloc()
+    {
+        for (int pos{0}; pos < argc; ++pos) {
+            delete[] argv[pos];
+        }
+        delete[] argv;
+        argv = nullptr;
+        argc = 0;
+        return;        
+    }
+};
+
+
+TEST_F(CliTest, help)
+{
+    cmdl({"cppapp", "-h"});
+    ASSERT_EQ(cli(argc, argv), EXIT_SUCCESS);
+    return;
+}
