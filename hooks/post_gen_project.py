3bd161a87f98c2a3a358bf7f12f72463e00be4b9
Michael Klatt <mdklatt@alumni.ou.edu>
2019-06-03 23:17:08 -0500

    Refactored post_gen_project hook script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,19 +4,13 @@ This is executed in the new project directory. Before execution it is rendered
 with the template engine, so it has access to all template variables.
 
 """
-from os import renames
-from os.path import join
-from shutil import rmtree
-from tempfile import mkdtemp
-try:
-    from urllib.request import urlopen
-except ImportError:  
-    # Python 2 compatibilty.
-    from urllib2 import urlopen
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from urllib.request import urlopen
 from zipfile import ZipFile
 
 
-def get_gtest():
+def get_googletest():
     """ Download Google Test from GitHub.
     
     Google recommends that each project maintains its own version of this
@@ -24,33 +18,30 @@ def get_gtest():
     violations of the One-Definition Rule.
     
     """
-    download = "https://github.com/google/googletest/archive/{:s}.zip"
     release = "{{ cookiecutter.googletest_release }}"
-    print("downloading Google Test ({:s})".format(release))
+    download = f"https://github.com/google/googletest/archive/{release:s}.zip"
+    print(f"downloading Google Test ({release:s})")
     if release != "master":
-        release = "release-{:s}".format(release)
-    libroot = join("test", "lib", "gtest")
-    tmproot = mkdtemp()
-    try:
+        release = f"release-{release:s}"
+    libroot =  Path("test", "lib", "gtest")
+    libroot.mkdir(parents=True, exist_ok=True)
+    with TemporaryDirectory() as tmpdir:
         # Extract the downloaded zip file to a temporary directory. ZipFile
         # requires a stream with a seek() method, so the archive must be
         # downloaded to a local file first.
-        stream = open(join(tmproot, "gtest.zip"), "w+b")
-        print(download.format(release))
-        stream.write(urlopen(download.format(release)).read())
-        archive = ZipFile(stream, mode="r")
-        archive.extractall(tmproot)
-        renames(join(tmproot, "googletest-{:s}".format(release)), libroot)
-    finally:
-        rmtree(tmproot)
+        file = Path(tmpdir, "googletest.zip")
+        file.write_bytes(urlopen(download).read())
+        archive = ZipFile(file.open("r+b"), mode="r")
+        archive.extractall(tmpdir)
+        Path(tmpdir, f"googletest-{release:s}").replace(libroot)
     return
 
 
-def main():
+def main() -> int:
     """ Execute all tasks. 
     
     """
-    get_gtest()
+    get_googletest()
     return 0
 
 
